b7ef60eae7f1d809fdd71423dd74d3f6a4641f88
Tami Williams <tami@asktami.com>
2019-08-01 15:39:35 -0400

    Updates

---
+++ precache-manifest.fc48a6f8c8c04da3e7e3d0e19e7b5bea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2189e5d33a901a78d52acdcffc86d09f",
+    "url": "/react-trelloyes/index.html"
+  },
+  {
+    "revision": "5e84f3da02334721dedd",
+    "url": "/react-trelloyes/static/css/main.984ace5c.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-trelloyes/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "5e84f3da02334721dedd",
+    "url": "/react-trelloyes/static/js/main.da54cb73.chunk.js"
+  },
+  {
+    "revision": "658aba3b58d8e166956e",
+    "url": "/react-trelloyes/static/js/runtime~main.bc44e323.js"
+  }
+]);
